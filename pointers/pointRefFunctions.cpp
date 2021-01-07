25f87966527287775e95ee2aa73013e3bf0b17b1
mascaaj_u18dt810 <alokejm@gmail.com>
2021-01-07 17:24:22 -0600

    finish pointers and ref module

---
+++ pointers/pointRefFunctions.cpp
@@ -0,0 +1,32 @@
+// understanding data storage
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void add(int& a, int* b)
+{cout << "Total " << (a + *b) << endl;};
+
+int main()
+{
+    int num =100,sum=500;
+    int& ref = num;
+    int* ptr = &num;
+    void(* fn) (int& a, int* b) = add;
+    cout << "Reference  : " << ref << endl;
+    cout << "Pointer    : " << *ptr << endl;
+    cout << "Pointer Add: " << ptr << endl;    
+    fn(ref,ptr);
+    ptr = &sum;
+    cout << "Reference  : " << ref << endl;
+    cout << "Pointer    : " << *ptr << endl;
+    cout << "Pointer Add: " << ptr << endl;    
+    fn(ref,ptr);
+    ptr = &num;
+    cout << "Reference  : " << ref << endl;
+    cout << "Pointer    : " << *ptr << endl;
+    cout << "Pointer Add: " << ptr << endl;    
+    fn(ref,ptr);
+}
+
+
--- pointers/pointerArrays.cpp
+++ pointers/pointerArrays.cpp
@@ -9,20 +9,25 @@ void computeTriple(int*);
 
 int main()
 {
-    int num = 5;
-    int* ptr = &num;
-    cout << "Variable memory address :" << &num << endl;
-    cout << "Pointer memory address :" << ptr << endl;
-    writeOutput(ptr);
-    computeTriple(ptr);
-    writeOutput(ptr);
-    cout << "Pointer memory address :" << ptr << endl;
-    *ptr+=15;
-    writeOutput(ptr);
-    computeTriple(ptr);
-    writeOutput(ptr);
-    cout << "Pointer memory address :" << ptr << endl;
+    char letter[8] = {'C','+','+',' ','F','u','n','\0'};
+    const char* text = "C++ Fun";
+    const char* term = "Element";
+    const char* lang = "C++";
+    const char* ap1[3] = {"Great","Program","Code "};
+    const char* ap2[3] = {lang,"is","Fun"};
+    const char* ap3[3] = {ap2[0],ap2[1],ap1[0]};
+    const char* ap4[3] = {ap1[2],ap2[1],ap2[2]};
 
+    cout << letter << endl;
+    cout << text << endl;
+    for (int i=0; i<3;i++ )
+    {
+        cout << term << i << " ";
+        cout << ap1[i] << " " ;
+        cout << ap2[i] << " " ;
+        cout << ap3[i] << " " ;
+        cout << ap4[i] << " " << endl;
+    }
 }
 
 void writeOutput(int* num)
