26eaa396fe58e9410502295bb9bba2a9fd08f2cd
aloke <boo@boodev.com>
2021-10-27 16:12:44 -0500

    Add gtest example and action yaml

---
+++ gtestExample/src/whattotest.cpp
@@ -0,0 +1,11 @@
+// Basic gtest example
+#include<math.h>
+
+double squareRoot(const double a) {
+    double b = sqrt(a);
+    if (b != b){
+        return -1.0;
+    } else {
+        return sqrt(a);
+    }
+}
